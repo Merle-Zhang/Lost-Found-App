0ab0c0ea70b8b112f0ff2d656984b4c0061df8a6
Merle Zhang <merle.zhang@qq.com>
2019-07-01 02:29:27 +0800

    Added: Buttom navigator

---
+++ lib/pages/setting.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:lost_found_app/services/authentication.dart';
+
+class SettingPage extends StatefulWidget {
+  SettingPage({Key key, this.auth, this.userId, this.onSignedOut}) : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+  final String userId;
+
+  @override
+  _SettingPageState createState() => new _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Setting'),
+        actions: <Widget>[
+          new FlatButton(
+              child: new Text('Logout',
+                  style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+              onPressed: widget.onSignedOut)
+        ],
+      ),
+      body: Text('Map'),
+    );
+  }
+}
